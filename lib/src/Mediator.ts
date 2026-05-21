b4857def715fe5fc660eca84a4708323141a49b6
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-05-22 01:21:54 +0200

    add minimal events

--- lib/src/Mediator.ts
+++ lib/src/Mediator.ts
@@ -23,6 +23,7 @@
 export default class MediatorTemplate extends Mediator<iEventsMinimal & {
         "initialized": [ ContainerPattern ];
         "released": [ ContainerPattern ];
+        "error": [ Error ];
     }> {
 
     // constructor
--- lib/src/Server.ts
+++ lib/src/Server.ts
@@ -3,13 +3,22 @@
     // externals
     import { Server } from "node-pluginsmanager-plugin";
 
+// types & interfaces
+
+    // locals
+    import type MediatorTemplate from "./Mediator";
+
 // module
 
 export default class ServerTemplate extends Server {
 
     public _initWorkSpace (): Promise<void> {
 
-        // (this._Mediator as MediatorTemplate).on(<event>, this._on<method>);
+        (this._Mediator as MediatorTemplate)
+
+            .on("initialized", this._onInitialized)
+            .on("released", this._onReleased)
+            .on("error", this._onError);
 
         return Promise.resolve();
 
@@ -17,12 +26,34 @@ export default class ServerTemplate extends Server {
 
     public _releaseWorkSpace (): Promise<void> {
 
-        // (this._Mediator as MediatorTemplate).off(<event>, this._on<method>);
+        (this._Mediator as MediatorTemplate)
+
+            .removeListener("initialized", this._onInitialized)
+            .removeListener("released", this._onReleased)
+            .removeListener("error", this._onError);
 
         return Promise.resolve();
 
     }
 
     // <events>
+
+    private readonly _onInitialized = (): void => {
+
+        this.push("initialized");
+
+    };
+
+    private readonly _onReleased = (): void => {
+
+        this.push("released");
+
+    };
+
+    private readonly _onError = (err: Error): void => {
+
+        this.push("error", err);
+
+    };
 
 }
